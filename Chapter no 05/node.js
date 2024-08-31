070067dc12518432d2f93d3b2cb301b2b2babdf9
zulfiqaraliwd <zulfiqaraliwd@gmail.com>
2024-09-01 02:03:02 +0500

    chapter no 5 Questions 06 completed

---
+++ Chapter no 05/node.js
@@ -0,0 +1,77 @@
+//Question no 01------------------------------------------------
+/*var a = 8;
+var b = 16;
+var c = a+b;
+
+document.write( "Sum of " + a + "  and "+ b + " is "+ c  );*/
+
+//Question no 02--------------------------------------------------------
+/*//multiplication
+var a = 8;
+var b = 16;
+var c = a*b;
+document.write( "Product of " + a + "  and "+ b + " is "+ c);
+
+//substraction
+var a = 8;
+var b = 16;
+var c = a-b;
+document.write( "difference of " + a + "  and "+ b + " is "+ c  );
+
+//division
+var a = 16;
+var b = 8;
+var c = a/b;
+
+document.write( "result of division " + a + "  and "+ b + " is "+ c  );
+*/
+
+
+//Question no 03----------------------------------------------------------------
+/*var a = 5;              //initial value
+a += 6;                 //increament by 6
+var b = a+13;           //add 13  
+b -= 6;                  // decrement the variable
+var c = b%2;
+
+console.log(c);*/
+
+
+
+//Question no 04---------------------------------------------------------------------
+
+/*
+var pkr = 600;
+var no_0f_ticket = 5;
+
+document.write("Total cost for ticket is : " + pkr * no_0f_ticket +" PKR") ;*/
+
+
+//Question no 05-----------------------------------------------------------------------------
+/*
+document.write("4 x 1 = " + (4 * 1) + "<br>");
+document.write("4 x 2 = " + (4 * 2) + "<br>");
+document.write("4 x 3 = " + (4 * 3) + "<br>");
+document.write("4 x 4 = " + (4 * 4) + "<br>");
+document.write("4 x 5 = " + (4 * 5) + "<br>");
+document.write("4 x 6 = " + (4 * 6) + "<br>");
+document.write("4 x 7 = " + (4 * 7) + "<br>");
+document.write("4 x 8 = " + (4 * 8) + "<br>");
+document.write("4 x 9 = " + (4 * 9) + "<br>");
+document.write("4 x 10 = " + (4 * 10) + "<br>");
+*/
+
+
+//Question no 06--------------------------------------------------------------------------------------
+//Ferenheiht into Celcius:
+/*var F = prompt("enter the temperature in Farenheight");
+var C = (F - 32)*5/9;
+document.write(C + "C");
+
+
+
+//Celcius into Ferenheiht:
+var C = prompt("enter the temperature in Celcius");
+var F = (C * 9/5)+32;
+document.write(F + "F");*/
+
