26624fd69c629fb4e24cc256a0c67ac04f5f0811
zulfiqaraliwd <zulfiqaraliwd@gmail.com>
2024-08-31 16:24:52 +0500

    chapter no 04 added

---
+++ Chapter no 04/node.js
@@ -0,0 +1,34 @@
+
+//Question no 01------------------------------------------------------------------
+/*var a = "variables names"
+var b = "can only contains"
+var c = "numbers"
+
+document.write(a + b + c);
+*/
+
+//Question no 02---------------------------------------------------------------------
+
+/*   //legal variable names
+var num1 = 3;
+var sum = 45;
+var tatal_marks = 30;
+var userName = "Zulfiqar ali";
+var $_name = "ali";
+
+//illegal variable names
+var 1num = 3;
+var SuM = 45;
+var Total marks = 30;
+var Userame = "Zulfiqar ali";
+var 125 = "username is zulfiqaar ali"
+
+*/
+
+//Question no 03---------------------------------------------------------------------
+/*  a) var $my_1stVariable = "zulfiqar ali";
+    b) var $name = "abdullah";                   */
+
+
+//variables names are case sensitive .
+//we cannnot use varaible's name which are are JS keyword
